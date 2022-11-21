3ea6a4b0c2721631ee3bfc268c99b2ffdf4d8139
abrahamcabrera490 <55775782+abrahamcabrera490@users.noreply.github.com>
2022-11-21 16:31:38 -0600

    AppMusica

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CSMusicPlayer
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayer));
+            this.songList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // songList
+            // 
+            this.songList.FormattingEnabled = true;
+            this.songList.Location = new System.Drawing.Point(716, 16);
+            this.songList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.songList.Name = "songList";
+            this.songList.Size = new System.Drawing.Size(132, 381);
+            this.songList.TabIndex = 0;
+            this.songList.DoubleClick += new System.EventHandler(this.songList_DoubleClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(718, 411);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(60, 24);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(788, 411);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(60, 24);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(26, 16);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(659, 387);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 509);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.songList);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MusicPlayer";
+            this.Text = "Media Player ARC";
+            this.Load += new System.EventHandler(this.MusicPlayer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSMusicPlayer
+{
+    public partial class MusicPlayer : Form
+    {
+        /*
+        List<string> _songNames;
+        List<string> _songPaths;
+        */
+        List<Song> _songs;
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void MusicPlayer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            
+            dialog.Multiselect = true;
+
+            if(dialog.ShowDialog() == DialogResult.OK )
+            {
+
+
+                AddSongsTolist(dialog.SafeFileNames.ToList(), dialog.FileNames.ToList());
+
+                //Añadir musica a la lista
+
+            }
+
+
+        }
+
+
+        private void AddSongsTolist(List<string> names, List<string> paths)
+        {
+
+            if(_songs == null)
+            {
+                _songs = new List<Song>();
+            }
+
+         
+            foreach (var item in names)
+            {
+
+                //Comprobamos si existe o no
+
+                if (!ExistOnList(item))
+                    _songs.Add(new Song(item, GetPath(item, paths)));
+            }
+
+            RefreshList();
+        }
+
+
+        private string GetPath(string fileName, List<string> songPath = null)
+        {
+            string actualpath = string.Empty;
+
+
+
+        if(songPath == null )
+
+            {
+
+                foreach (var songs in _songs)
+                {
+                    if (songs.Name == fileName)
+                    {
+                        actualpath = songs.Path;
+                    }
+                }
+
+            }
+            
+        else { 
+
+            foreach(var path in songPath)
+            {
+                if(path.Contains(fileName))
+                {
+                    actualpath = path;
+               
+                    }
+            }
+            }
+            return actualpath;
+        }
+
+
+        private bool ExistOnList(string song)
+        {
+
+            bool exist = false;
+            foreach (var item in _songs)
+            {
+                if(item.Name == song)
+                {
+                  exist = true;
+                }
+            }
+            return exist;
+
+        }
+
+
+        private void songList_DoubleClick(object sender, EventArgs e)
+        {
+
+
+
+            axWindowsMediaPlayer1.URL = GetPath(songList.Text);
+
+
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            Song songRemobe = null;
+
+            foreach(var song in _songs)
+            {
+                if (song.Name == songList.Text)
+                {
+                    songRemobe = song;
+                }
+            }
+            if(songRemobe != null)
+                _songs.Remove(songRemobe);
+
+
+            RefreshList();
+        }
+
+
+        private void RefreshList()
+        {
+            List<string> songNames = new List<string>();
+
+            foreach(var item in _songs)
+            {
+                songNames.Add(item.Name);
+            }
+            songList.DataSource = null;
+            songList.DataSource = songNames;
+        }
+    }
+}
